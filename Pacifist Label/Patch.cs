b2c02d2d402350540ede7d8aaf49a6dd68e4e6db
DragonFire47 <1redyoung@gmail.com>
2021-03-16 19:10:01 -0400

    Add project files.

---
+++ Pacifist Label/Patch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace Pacifist_Label
+{
+    [HarmonyPatch(typeof(PLInGameUI), "Update")]
+    class Patch
+    {
+        static void Postfix()
+        {
+            if (PLServer.Instance != null && PLServer.Instance.PacifistRun)
+            {
+                PLGlobal.SafeLabelSetText(PLInGameUI.Instance.CurrentVersionLabel, $"{PLInGameUI.Instance.CurrentVersionLabel.text} (Pacifist)");
+            }
+        }
+    }
+}
---
+++ Pacifist Label/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace Pacifist_Label
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string LongDescription => "Shows whether current run is pacifist";
+
+        public override string Name => "PacifistLabel";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.PacifistLabel";
+        }
+    }
+}
